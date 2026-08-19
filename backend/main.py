b45614562b768439fe5c8d27a1c7b7dae9e9c4ff
luigicalima-hash <luigicalima@gmail.com>
2026-08-19 15:26:18 -0300

    fix: importa o Field do pydantic no main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import sqlite3
 from datetime import date
 
